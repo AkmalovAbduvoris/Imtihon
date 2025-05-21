5d017fd8c57bc97f6742612d9b000c3db2cb559c
AkmalovAbduvoris <abduvorisakmalov5005@gmail.com>
2025-05-21 19:23:15 +0500

    PATCH: added footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a href="https://documenter.getpostman.com/view/43114536/2sB2qZFhhH#83705c1f-bb7f-49f2-9993-7e010da2a8c3" target="_blank">Postman</a>
+                <a href="https://github.com/AkmalovAbduvoris/Imtihon" target="_blank">Github</a>
+                <p>
+                    For suggestions or complaints:
+                    <a href="https://t.me/Abduvorisss" target="_blank">Contact us on Telegram</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
     <div class="container">
         @yield('content')
     </div>
+    <x-footer />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
